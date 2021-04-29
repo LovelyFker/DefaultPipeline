f4ff0890a676a72838932db382c099d061ffa8f1
LovelyFker <252912901@qq.com>
2021-04-29 18:09:05 +0800

    修正编辑器插件脚本位置；添加KeyCode测试脚本及场景；

---
+++ Assets/Scripts/Test/KeyCodeReceiveTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyCodeReceiveTest : MonoBehaviour
+{
+    public Text mText;
+    public List<string> KeyCodeInfo = new List<string>();
+    [Range(10, 20)]
+    public int mMaxShowElement = 15;
+
+    private int mCurrentIndex = 0;
+
+    private void Start()
+    {
+        mCurrentIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mText.text = "";
+
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    if (KeyCodeInfo.Count < mMaxShowElement)
+                        KeyCodeInfo.Add(keyCode.ToString());
+                    else
+                    {
+                        KeyCodeInfo.RemoveAt(0);
+                        KeyCodeInfo.Add(keyCode.ToString());
+                    }
+                }
+            }
+        }
+
+        foreach (string s in KeyCodeInfo)
+        {
+            mText.text += s + "\n";
+        }
+    }
+}
